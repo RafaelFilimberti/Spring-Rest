ba4575542c1375f6cd486d0aa34eedf86526573e
Rafael Filimberti <rfilimberti@gmail.com>
2025-02-07 16:01:50 -0300

    3.15. Conhecendo e usando o Lombok

--- algafood-api/src/main/java/com/algaworks/algafood/jpa/InclusaoCozinhaMain.java
+++ algafood-api/src/main/java/com/algaworks/algafood/jpa/InclusaoCozinhaMain.java
@@ -8,22 +8,31 @@ import org.springframework.context.ApplicationContext;
 
 import com.algaworks.algafood.AlgafoodApiApplication;
 import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.repository.CozinhaRepository;
 
 public class InclusaoCozinhaMain {
 
 	public static void main(String[] args) {
-		ApplicationContext applicationContext = new SpringApplicationBuilder(AlgafoodApiApplication.class) //inicia a aplicação sem ser de forma Web
-				.web(WebApplicationType.NONE)
-				.run(args);
-		
-		CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class); //vai retornar o bean CadastroCozinha
-		
-		List<Cozinha> cozinhas = cadastroCozinha.Listar();
-		
-		
-		for (Cozinha cozinha : cozinhas) {
-			System.out.println(cozinha.getNome());
-		}
-		
-		}
+		ApplicationContext applicationContext = new SpringApplicationBuilder(AlgafoodApiApplication.class) // inicia a
+																											// aplicação
+																											// sem ser
+																											// de forma
+																											// Web
+				.web(WebApplicationType.NONE).run(args);
+
+		CozinhaRepository cozinhaRepository = applicationContext.getBean(CozinhaRepository.class);
+
+		Cozinha cozinha1 = new Cozinha();
+		cozinha1.setNome("Brasileira");
+
+		Cozinha cozinha2 = new Cozinha();
+		cozinha2.setNome("Japonesa");
+
+		cozinha1 = cozinhaRepository.salvar(cozinha1);
+		cozinha2 = cozinhaRepository.salvar(cozinha2);
+
+		System.out.printf("%d - %s\n", cozinha1.getId(), cozinha1.getNome());
+		System.out.printf("%d - %s\n", cozinha2.getId(), cozinha2.getNome());
+
+	}
 }
